4504c9cd40bb045e1066a7f62860947f1fad25ff
maruscak.jan <maruscak.jan@ssakhk.cz>
2020-10-21 15:47:10 +0200

    navbar

--- ClientApp/src/App.vue
+++ ClientApp/src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
+    <Navbar />
     <router-view/>
   </div>
 </template>
+
+<script>
+
+import Navbar from "./components/Navbar.vue";
+export default {
+  name: "App" ,
+  components: {
+    Navbar
+  }
+}
+</script>>
 
 <style lang="scss">
 #app {
--- ClientApp/src/components/Navbar.vue
+++ ClientApp/src/components/Navbar.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>this is landing page</div>
+  <nav class="main-wrapper">
+    <router-link class="nav-primary" to="/">Školu nepobírám</router-link>
+    <div class="nav-secondary">
+      <router-link to="/hledat" class="btn-s btn-primary"
+        >Hledat školu</router-link
+      >
+      <router-link to="/hledat" class="btn-s btn-primary">Mapa</router-link>
+      <router-link to="/hledat" class="btn-s btn-primary"
+        >Aktuální opatření</router-link
+      >
+      <router-link to="/hledat" class="btn-s btn-secondary"
+        >Přihlášení školy</router-link
+      >
+    </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -8,3 +22,24 @@ export default Vue.extend({
   name: "Navbar",
 });
 </script>
+
+<style scoped lang="scss">
+nav {
+  height: 72px;
+  .nav-primary {
+    margin-right: 400px;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  .nav-secondary {
+    display: inline-block;
+    a{
+      margin-right: 10px;
+    }
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+</style>

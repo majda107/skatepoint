36bf60d7f542f4b1dcb9032bae576e5f3ad73cad
majda107 <majda.trpkos@gmail.com>
2020-10-22 00:05:32 +0200

    working user points

---
+++ Migrations/20201021220236_user model.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace skolu_nepobiram.Migrations
+{
+    public partial class usermodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "SkatePlaces",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SkatePlaces_ApplicationUserId",
+                table: "SkatePlaces",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SkatePlaces_AspNetUsers_ApplicationUserId",
+                table: "SkatePlaces",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SkatePlaces_AspNetUsers_ApplicationUserId",
+                table: "SkatePlaces");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SkatePlaces_ApplicationUserId",
+                table: "SkatePlaces");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "SkatePlaces");
+        }
+    }
+}

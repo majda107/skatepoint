70bb896abefd7b723f09d3151ae5db315c145d2d
majda107 <majda.trpkos@gmail.com>
2020-10-22 00:01:43 +0200

    auth methods

--- Controllers/SkateController.cs
+++ Controllers/SkateController.cs
@@ -1,6 +1,9 @@
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using skolu_nepobiram.Database;
 using skolu_nepobiram.Database.Models;
 
@@ -9,17 +12,28 @@ namespace skolu_nepobiram.Controllers
     public class SkateController : Controller
     {
         private readonly DatabaseContext _db;
+        private readonly IHttpContextAccessor _accessor;
 
-        public SkateController(DatabaseContext db)
+        public SkateController(DatabaseContext db, IHttpContextAccessor accessor)
         {
             this._db = db;
+            this._accessor = accessor;
         }
 
         [HttpPost]
+        [Authorize]
         public async Task<IActionResult> AddPoint([FromBody] SkatePlace place)
         {
             if (!ModelState.IsValid) return BadRequest();
+            var user = this._accessor.HttpContext.User;
+            var userEntry = this._db.Users.Include(u => u.Places).FirstOrDefault(u => u.UserName == user.Identity.Name);
+
+            if (userEntry == null) return Unauthorized();
+
             this._db.Add(place);
+            await this._db.SaveChangesAsync();
+
+            userEntry.Places.Add(place);
             await this._db.SaveChangesAsync();
 
             return new JsonResult(place);
@@ -29,6 +43,19 @@ namespace skolu_nepobiram.Controllers
         public async Task<IActionResult> GetPoints()
         {
             return new JsonResult(this._db.SkatePlaces);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetMyPoints()
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var user = this._accessor.HttpContext.User;
+            var userEntry = this._db.Users.Include(u => u.Places).FirstOrDefault(u => u.UserName == user.Identity.Name);
+
+            if (userEntry == null) return Unauthorized();
+
+            return new JsonResult(userEntry.Places);
         }
 
         [HttpDelete]

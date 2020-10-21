b860d779ddd41c5fc26c3fcf583cc27080ba8d9f
majda107 <majda.trpkos@gmail.com>
2020-10-21 15:48:52 +0200

    added poppins font

--- ClientApp/src/App.vue
+++ ClientApp/src/App.vue
@@ -4,12 +4,17 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
 @import "../styles/main.scss";
+
+* {
+  font-family: "Poppins", sans-serif;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;

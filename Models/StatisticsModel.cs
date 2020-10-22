e4ba59ccffdbff2245ffcc9ff6ee0e025e0e3677
majda107 <majda.trpkos@gmail.com>
2020-10-22 12:16:43 +0200

    statistics model

---
+++ Models/StatisticsModel.cs
@@ -0,0 +1,10 @@
+namespace skolu_nepobiram.Models
+{
+    public class StatisticsModel
+    {
+        public int SpotCount { get; set; }
+        public int UserCount { get; set; }
+        public int LikeCount { get; set; }
+        public int PlaceCount { get; set; }
+    }
+}

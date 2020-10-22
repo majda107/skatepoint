af5f3e02a113920edf3fec45c4a75cef388f814d
maruscak.jan <maruscak.jan@ssakhk.cz>
2020-10-22 09:24:17 +0200

    docs view

---
+++ ClientApp/src/views/DocumentationView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-wrapper">
+    <div class="doc">
+      <div class="doc-header">Hello World</div>
+      <div class="doc-about">
+        Let's start by testing our setup. Open up a command prompt and enter the
+        following command:
+      </div>
+      <pre><code class="typescript">export default Vue.extend({});</code></pre>
+      <pre class="doc-code">
+curl https://api.github.com/zen
+Keep it logically awesome.</pre
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/main.scss";
+</style>
